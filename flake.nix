989f6fee143685187141b6f250e4736487d8a646
Clément Songis <5clemans585@gmail.com>
2025-09-19 20:48:18 +0200

    Use flake utils

--- flake.nix
+++ flake.nix
@@ -3,17 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self, nixpkgs, ... }:
     {
-      nixosModules = {
-        refind = import ./modules/refind.nix;
-        default = self.nixosModules.refind;
-      };
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      {
+        nixosModules = {
+          refind = import ./modules/refind.nix;
+          default = self.nixosModules.refind;
+        };
 
-      packages.x86_64-linux.refind = nixpkgs.legacyPackages.x86_64-linux.refind;
-      packages.x86_64-linux.default = self.packages.x86_64-linux.refind;
-    };
+        packages = {
+          refind = nixpkgs.legacyPackages.${system}.refind;
+          default = self.packages.${system}.refind;
+        };
+      }
+    );
 }
