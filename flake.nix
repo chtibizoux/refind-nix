8424a49cd48a989a63e231f0736a13e01a66c782
Clément Songis <5clemans585@gmail.com>
2025-09-20 18:13:57 +0200

    Simplify flake

--- flake.nix
+++ flake.nix
@@ -1,30 +1,12 @@
 {
   description = "rEFInd bootloader flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
   outputs =
+    { self }:
     {
-      self,
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      {
-        nixosModules = {
-          refind = import ./modules/refind.nix;
-          default = self.nixosModules.refind;
-        };
-
-        packages = {
-          refind = nixpkgs.legacyPackages.${system}.refind;
-          default = self.packages.${system}.refind;
-        };
-      }
-    );
+      nixosModules = {
+        refind = import ./modules/refind.nix;
+        default = self.nixosModules.refind;
+      };
+    };
 }
